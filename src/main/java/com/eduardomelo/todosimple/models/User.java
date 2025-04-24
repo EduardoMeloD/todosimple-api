89585a5b462a54685132cfb381f7ab87482eb067
eduardo de melo <duduperuano@outlook.com>
2025-04-24 15:36:43 -0300

    criando a classe de task

--- src/main/java/com/eduardomelo/todosimple/models/User.java
+++ src/main/java/com/eduardomelo/todosimple/models/User.java
@@ -1,13 +1,14 @@
 package com.eduardomelo.todosimple.models;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -18,7 +19,8 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import java.util.Objects;
 
-//import org.springframework.scheduling.config.Task;
+import com.eduardomelo.todosimple.models.Task;
+
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -33,6 +35,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
+    
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotEmpty(groups = CreateUser.class)
     @NotNull(groups = CreateUser.class)
@@ -46,7 +49,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -81,6 +85,15 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     
     @Override
     public boolean equals (Object obj){
