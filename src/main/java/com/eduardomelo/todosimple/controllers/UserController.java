1d441b2de99fd42427cb090d061391cf059a158c
eduardo de melo <duduperuano@outlook.com>
2025-05-23 16:34:24 -0300

    criaçao do UserController

--- src/main/java/com/eduardomelo/todosimple/controllers/UserController.java
+++ src/main/java/com/eduardomelo/todosimple/controllers/UserController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eduardomelo.todosimple.models.User;
 import com.eduardomelo.todosimple.models.User.CreateUser;
+import com.eduardomelo.todosimple.models.User.UpdateUser;
 import com.eduardomelo.todosimple.services.UserService;
 
 @RestController
@@ -40,5 +43,19 @@ public class UserController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
         .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated(UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id){
+        obj.setId(id);
+        this.userService.AtualizarDados(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
